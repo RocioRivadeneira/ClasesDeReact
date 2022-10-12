8c237527b395a87a6c60baec71791be5519b9e19
Rocio Rivadeneira <rociorivadeneira2@gmail.com>
2022-10-12 05:45:53 -0300

    cart view tp

--- src/components/CartView/CartView.jsx
+++ src/components/CartView/CartView.jsx
@@ -1,23 +1,24 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { cartCtx } from "../../context/cartContext";
 import ItemCount from "../ItemCount/Count";
 
 import "./cartView.css";
 
 function CartView() {
-    const [setIsInCart] = useState(false);
     const { cart, addItem, removeItem } = useContext(cartCtx);
 
-    let carritovacio = false;
-
-    if (carritovacio) {
-        return <div>Tu carrito está vacio...</div>;
+    if (!cart.length) {
+        return (
+            <div class="text-center my-2 h4">
+                Tu carrito está vacio... <Link to="/">Ver productos</Link>{" "}
+            </div>
+        );
     }
 
     function handleAddToCart(item) {
         return (quantity) => {
             addItem(item, quantity);
-            setIsInCart(true);
         };
     }
 
@@ -25,20 +26,21 @@ function CartView() {
         <div>
             {cart.map((item) => (
                 <div>
-                    <table className="table text-center">
+                    <table className="text-center">
                         <tr>
                             <th>Producto</th>
                             <th>Nombre</th>
                             <th>Detalles</th>
                             <th>Precio</th>
                             <th>Unidades</th>
-                            <th>Total</th>
-                            <th>Eliminar</th>
                             <th>Cantidad</th>
+                            <th>Eliminar</th>
+                            <th>Total</th>
                         </tr>
                         <tr>
                             <td>
-                                <img className="img"
+                                <img
+                                    className="img"
                                     src={item.img}
                                     alt={item.title}
                                 />
@@ -47,22 +49,22 @@ function CartView() {
                             <td>{item.detail}</td>
                             <td>$ {item.price}</td>
                             <td>{item.quantity}</td>
-                            <td>{item.price * item.quantity}</td>
-                            <td>
-                                <button
-                                    type="button"
-                                    className="btn btn-link text-decoration-none mt-2"
-                                    onClick={() => removeItem(item)}
-                                >
-                                    Eliminar
-                                </button>
-                            </td>
                             <td>
                                 <ItemCount
                                     stockLimit={item.stock}
                                     onAddToCart={handleAddToCart(item)}
                                 />
                             </td>
+                            <td>
+                                <button
+                                    type="button"
+                                    className="btn btn-link text-decoration-none mt-2"
+                                    onClick={() => removeItem(item)}
+                                >
+                                    X
+                                </button>
+                            </td>
+                            <td>{item.price * item.quantity}</td>
                         </tr>
                     </table>
                 </div>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,20 +2,15 @@ import React, { useContext, useState } from "react";
 import { cartCtx } from "../../context/cartContext";
 import FlexWrapper from "../FlexWrapper/FlexWrapper";
 import ItemCount from "../ItemCount/Count";
-import { Link } from "react-router-dom";
+
 
 import "./item.css";
 
 function ItemDetail(item) {
-    const [isInCart, setIsInCart] = useState(false);
-    const { addItem, removeItem } = useContext(cartCtx);
+    const { addItem } = useContext(cartCtx);
 
     function handleAddToCart(quantity) {
         addItem(item, quantity);
-    }
-
-    function handleItemDelete() {
-        removeItem(item);
     }
 
     return (
@@ -29,22 +24,10 @@ function ItemDetail(item) {
                         <h4 className="card-price">$ {item.price}</h4>
                         <strong>Stock Disponible: {item.stock}</strong>
 
-                        {isInCart ? (
-                            <Link to="/cart">Ir al carrito</Link>
-                        ) : (
-                            <ItemCount
-                                stockLimit={item.stock}
-                                onAddToCart={handleAddToCart}
-                            />
-                        )}
-
-                        <button
-                            type="button"
-                            className="btn btn-link text-decoration-none mt-2"
-                            onClick={handleItemDelete}
-                        >
-                            Quitar del carrito
-                        </button>
+                        <ItemCount
+                            stockLimit={item.stock}
+                            onAddToCart={handleAddToCart}
+                        />
                     </div>
                 </div>
             </div>
