0dcef20e8dee5c651b0bc34b738b24dccad7782b
Rocio Rivadeneira <rociorivadeneira2@gmail.com>
2022-10-12 12:10:50 -0300

    tp cartview cambios

--- src/components/ItemCount/Count.jsx
+++ src/components/ItemCount/Count.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 
 import "./count.css";
 
